b51bab58a71835002bd9188f9e7ac505c703ef06
DancewithPeng <dancewithpeng@gmail.com>
2017-03-09 22:50:30 +0800

    修改了头文件

--- DPMapper/DPMapper.h
+++ DPMapper/DPMapper.h
@@ -15,6 +15,3 @@ FOUNDATION_EXPORT double DPMapperVersionNumber;
 FOUNDATION_EXPORT const unsigned char DPMapperVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DPMapper/PublicHeader.h>
-
-
-// 哈哈哈
